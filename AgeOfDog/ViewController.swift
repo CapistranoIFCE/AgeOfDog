01a4bedeffcf749d9be25e2d764b87bbc45ea99d
CapistranoIFCE <101426741+CapistranoIFCE@users.noreply.github.com>
2022-07-09 08:31:36 -0300

    Atualização 9.Jul

--- AgeOfDog/ViewController.swift
+++ AgeOfDog/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var legendResult: UILabel!
+    @IBOutlet weak var campAgeDog: UITextField!
+    @IBAction func discoverAge(_ sender: Any) {
+        let age = Int(campAgeDog.text!)! * 7
+        legendResult.text = "A idade do Cachorro é: " + String(age)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("hello")
     }
 
 
